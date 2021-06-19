4c4288d25daaee26253dafc4099d5895238e2a08
mekaneeky <alizawahry1@gmail.com>
2021-06-19 19:18:26 +0200

    Minor processor_class bugfix

--- utils/processor_class.py
+++ utils/processor_class.py
@@ -1,17 +1,19 @@
 
 import os
+from visualizers import generate_mel_spectogram
 
 class DataProcessor():
 
     def __init__(self, main_data_dir, val_dir = None, test_dir= None, do_train_val_test_split = False):
         self.main_dir = main_data_dir
         self.val_dir = val_dir
-        self.test_dir = test_dir        
+        self.test_dir = test_dir  
+        self.initialize_files()      
         #self.convert_ogg2wav()
         #self.generate_dir_spectrograms()
 
 
-    def convert_ogg2wav():
+    def initialize_files(self):
 
         self.train_wavfiles = [os.path.join(root, name)
             for root, dirs, files in os.walk(self.main_dir)#"data/nlp_keyword_bucket/train_1/")
@@ -32,12 +34,12 @@ class DataProcessor():
                 for name in files
                 if name.endswith((".wav"))]
 
+    def convert_ogg2wav(self):
         for filename in self.train_wavfiles:
             words = re.findall(r"[\w']+", filename)
             os.system("ffmpeg -y -i {0} {0}".format(filename))
 
         if self.val_dir:
-
             for filename in self.val_wavfiles:
                 words = re.findall(r"[\w']+", filename)
                 os.system("ffmpeg -y -i {0} {0}".format(filename))
@@ -49,25 +51,6 @@ class DataProcessor():
 
 
     def generate_dir_spectrograms(self,main_dir, val_dir = None, test_dir = None):
-
-        #train_wavfiles = [os.path.join(root, name)
-         #   for root, dirs, files in os.walk(main_dir)#"data/nlp_keyword_bucket/train_1/")
-          #  for name in files
-           # if name.endswith((".wav"))]
-
-        #if val_dir:
-
-            #val_wavfiles = [os.path.join(root, name)
-             #   for root, dirs, files in os.walk(val_dir)#"data/nlp_keyword_bucket/val_1/")
-              #  for name in files
-               # if name.endswith((".wav"))]
-        
-        #if test_dir:
-
-         #   test_wavfiles = [os.path.join(root, name)
-          #      for root, dirs, files in os.walk(test_dir)#"data/nlp_keyword_bucket/test_1/")
-           #     for name in files
-           #     if name.endswith((".wav"))]
 
         for train_wavfl in self.train_wavfiles:
           generate_mel_spectogram(train_wavfl)
